ea1588e8a9dfb61d5daae51d277f6fe9994ed7ae
450Oucema <45Ooucema@gmail.com>
2020-05-17 08:03:50 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import GuessCount from "./GuessCount";
+import Card from "./Card";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  handleCardClick(card) {
+    console.log(card + 'clicked');
+  }
+  render() {
+    return (
+        <div className="memory">
+          <GuessCount guesses={0}/>
+          <Card card="😇" feedback="hidden" onClick={this.handleCardClick()}/>
+          <Card card="😎" feedback="justMatched" onClick={this.handleCardClick()}/>
+          <Card card="😕" feedback="justMisMatched" onClick={this.handleCardClick()}/>
+          <Card card="😆" feedback="visible" onClick={this.handleCardClick()}/>
+          <Card card="👺" feedback="hidden" onClick={this.handleCardClick()}/>
+          <Card card="👻" feedback="visible" onClick={this.handleCardClick()}/>
+          <Card card="⛑" feedback="hidden" onClick={this.handleCardClick()}/>
+        </div>
+    )
+  }
 }
 
-export default App;
+export default App
